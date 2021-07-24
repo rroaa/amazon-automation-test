a1bf721f10dd550e09f6a49a3f3389aa4fbd39a7
Roaa Alnufaie <Roaa.alotaibi@hotmail.com>
2021-07-24 20:48:16 +0300

    Update AmazonHomePage.java

--- AmazonTest/src/pages/AmazonHomePage.java
+++ AmazonTest/src/pages/AmazonHomePage.java
@@ -28,7 +28,7 @@ public class AmazonHomePage{
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/AmazonProducts","root","root");
 			
-			// Execute retrieve products list query
+			// Retrieve products list from the db 
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from products");
 			
